6c68c1ec7ab5b9ff7291c07b8d37e6dd31f4e38a
Amir Hasan <ahasan0456@bths.edu>
2017-10-27 12:31:20 -0400

    Added Board and Runner files.

---
+++ AHasanTBA/src/Board.java
@@ -0,0 +1,12 @@
+/*
+*	Author: Amir Hasan
+*	Last Updated: 10-27-17
+*	Description: Board models the area for the user to move around.
+*/
+public class Board
+{
+	public Board()
+	{
+		
+	}
+}
---
+++ AHasanTBA/src/Runner.java
@@ -0,0 +1,12 @@
+/*
+*	Author: Amir Hasan
+*	Last Updated: 10-27-17
+*	Description: This is the file to run the project. Obviously.
+*/
+public class Runner
+{
+	public static void main(String[] args)
+	{
+		
+	}
+}
