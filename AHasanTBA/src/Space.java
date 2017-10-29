fb3c75b6a8b62cb7d23d049105e550a8bb28e53e
ahasan0456 <32273169+ahasan0456@users.noreply.github.com>
2017-10-29 18:26:21 -0400

    Delete Space.java

--- AHasanTBA/src/Space.java
+++
@@ -1,9 +0,0 @@
-/*
-*	Author: Amir Hasan
-*	Last Updated: 10-27-17
-*	Description: This is the file to run the project. Obviously.
-*/
-public interface Space
-{
-	public abstract void spaceEffect();
-}
